dbf8a0439846902bff661093943ffe620195035b
davutbilgic <db.davutbilgic@gmail.com>
2020-05-23 19:36:38 +0200

    update

--- src/test/java/com/cybertek/pages/CalendarEventsPage.java
+++ src/test/java/com/cybertek/pages/CalendarEventsPage.java
@@ -1,9 +1,14 @@
 package com.cybertek.pages;
 
+import com.cybertek.tests.TestBase;
+import com.cybertek.utilities.BrowserUtils;
 import com.cybertek.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 
 public class CalendarEventsPage extends BasePage {
 
@@ -14,16 +19,90 @@ public class CalendarEventsPage extends BasePage {
     @FindBy(css = "[title='Create Calendar event']")
     public WebElement createCalendarEvent;
 
-    //I added this for test1(), TestCase3
     @FindBy(xpath = "//div[@href='#']")
     public WebElement options;
 
-    //I added this for test2(), TestCase3
     @FindBy(xpath = "//input[@value='1']")
     public WebElement pageNumber;
 
-    //I added this for test3(), TestCase3
     @FindBy(css = "button[data-toggle='dropdown'][class='btn dropdown-toggle ']")
-    public WebElement pageNumber25;
+    public WebElement viewPerPageNumber;
+
+    @FindBy(xpath = "//button[@data-toggle='dropdown'][@class='btn dropdown-toggle ']")
+    public WebElement viewPerPage;
+
+    @FindBy(xpath = "//a[@data-size='100']")
+    public WebElement viewPerPageSelect100;
+
+//    @FindBy(css = "tr.grid-row")
+//    public WebElement rows;
+
+    @FindBy(xpath = "//label[starts-with(text(), 'Total')]")
+    public WebElement textOFnumberOfEvents;
+
+    @FindBy(xpath = "//button[@data-toggle='dropdown']/input[@type='checkbox']")
+    public WebElement checkboxClickableAll;
+
+
+    public int calculateNumberOfEvents() {
+        viewPerPage.click();
+        viewPerPageSelect100.click();
+        int selectedRowsNumber = Integer.parseInt(viewPerPageNumber.getText());
+
+        String pathRows = null;
+        int countPageNumber = 0;
+        for (int i = 1; i <= selectedRowsNumber; i++) {
+            pathRows = "tr.grid-row:nth-of-type(" + i + ")";
+
+            try {
+                if (!(Driver.get().findElement(By.cssSelector(pathRows)).equals(null))) {
+                    countPageNumber++;
+                }
+
+                if (i == selectedRowsNumber) {
+                    Driver.get().findElement(By.cssSelector(".fa-chevron-right.hide-text")).click();
+                    waitUntilLoaderScreenDisappear();
+                    i = 0;
+                }
+            } catch (NoSuchElementException e) {//NoSuchElementException coming from org.openga.selenium.
+                //System.out.println(e.toString());
+                return countPageNumber;
+            }
+        }
+        return countPageNumber;
+    }
+
+
+    public void verifyAllCheckbox() {
+        viewPerPage.click();
+        viewPerPageSelect100.click();
+
+        int selectedRowsNumber = Integer.parseInt(viewPerPageNumber.getText());
+
+        if (checkboxClickableAll.isSelected()) {
+            try {
+                String pathChecksboxOfEveryRows = null;
+                for (int i = 1; i <= selectedRowsNumber; i++) {
+                    pathChecksboxOfEveryRows = "tr.grid-row:nth-of-type(" + i + ")>td>input";
+                    if (!(Driver.get().findElement(By.cssSelector(pathChecksboxOfEveryRows)).isSelected())) {
+                        Assert.assertTrue(Driver.get().findElement(By.cssSelector(pathChecksboxOfEveryRows)).isSelected());
+                    }
+
+                    if (i == selectedRowsNumber) {
+                        Driver.get().findElement(By.cssSelector(".fa-chevron-right.hide-text")).click();
+                        waitUntilLoaderScreenDisappear();
+                        i = 0;
+                    }
+                }
+            } catch (NoSuchElementException e) {
+                Assert.assertTrue(checkboxClickableAll.isSelected());
+            }
+
+        } else {
+            Assert.assertTrue(checkboxClickableAll.isSelected());
+        }
+
+
+    }
 
 }
--- src/test/java/com/cybertek/tests/homeworks/TestCase3.java
+++ src/test/java/com/cybertek/tests/homeworks/TestCase3.java
@@ -6,6 +6,7 @@ import com.cybertek.pages.LoginPage;
 import com.cybertek.tests.TestBase;
 import com.cybertek.utilities.BrowserUtils;
 import com.cybertek.utilities.ConfigurationReader;
+import com.cybertek.utilities.Driver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -73,7 +74,7 @@ public class TestCase3 extends TestBase {
     }
 
     @Test
-    public void test3(){
+    public void test3() {
         /*
             1. Go to “https://qa1.vytrack.com/"
             2. Login as a store manager
@@ -95,21 +96,88 @@ public class TestCase3 extends TestBase {
         extentLogger.info("Navigate to “Activities -> Calendar Events”");
         new DashboardPage().navigateToModule("Activities","Calendar Events");
 
-        BrowserUtils.waitFor(6);//net yavas oldugu icin biraz daha beklemesini istedim
-        Assert.assertEquals(new CalendarEventsPage().pageNumber25.getText(),"25");
+        new CalendarEventsPage().waitUntilLoaderScreenDisappear();//net yavas oldugu icin biraz daha beklemesini istedim
+
+        extentLogger.info("Verify that view per page number is equals to '25'");
+        Assert.assertEquals(new CalendarEventsPage().viewPerPageNumber.getText(),"25");
 
         extentLogger.pass("PASS: TestCase3-test3() Test");
     }
 
-//    @Test
-//    public void test4(){
-//
-//    }
-//
-//    @Test
-//    public void test5(){
-//
-//    }
+    @Test
+    public void test4() {
+        /*
+            1. Go to “https://qa1.vytrack.com/"
+            2. Login as a store manager
+            3. Navigate to “Activities -> Calendar Events”
+            4. Verify that number of calendar events (rows in the table) is equals to number of records
+         */
+        extentLogger = report.createTest("Test Case3");
+
+        LoginPage loginPage = new LoginPage();
+        CalendarEventsPage calendarEventsPage = new CalendarEventsPage();
+
+        String username = ConfigurationReader.get("storemanager_username");
+        String password = ConfigurationReader.get("storemanager_password");
+
+        extentLogger.info("username: " + username);
+        extentLogger.info("password: " + password);
+        extentLogger.info("Login as a Store Manager");
+        loginPage.login(username,password);
+
+        extentLogger.info("Navigate to “Activities -> Calendar Events”");
+        new DashboardPage().navigateToModule("Activities","Calendar Events");
+
+        BrowserUtils.waitFor(5);
+        //calendarEventsPage.waitUntilLoaderScreenDisappear();
+
+        extentLogger.info("Verify that number of calendar events (rows in the table) is equals to number of records");
+        String totalRecordText = calendarEventsPage.textOFnumberOfEvents.getText();//Get text Total of .... Records
+        int expectedNumberOfEvents = Integer.valueOf(totalRecordText.replaceAll("[^0-9]", ""));//select number of in it
+        int actualNumberOfEvents = calendarEventsPage.calculateNumberOfEvents();
+        Assert.assertEquals(actualNumberOfEvents,expectedNumberOfEvents);
+
+        extentLogger.pass("PASS: TestCase3-test4() Test");
+    }
+
+    @Test
+    public void test5(){
+        /*
+            1. Go to “https://qa1.vytrack.com/"
+            2. Login as a store manager
+            3. Navigate to “Activities -> Calendar Events”
+            4. Click on the top checkbox to select all
+            5. Verify that all calendar events were selected
+         */
+
+        extentLogger = report.createTest("Test Case3");
+
+        LoginPage loginPage = new LoginPage();
+        CalendarEventsPage calendarEventsPage = new CalendarEventsPage();
+
+        String username = ConfigurationReader.get("storemanager_username");
+        String password = ConfigurationReader.get("storemanager_password");
+
+        extentLogger.info("username: " + username);
+        extentLogger.info("password: " + password);
+        extentLogger.info("Login as a Store Manager");
+        loginPage.login(username,password);
+
+        extentLogger.info("Navigate to “Activities -> Calendar Events”");
+        new DashboardPage().navigateToModule("Activities","Calendar Events");
+
+        //calendarEventsPage.waitUntilLoaderScreenDisappear();
+        BrowserUtils.waitFor(6);
+
+        extentLogger.info("Click on the top checkbox to select all");
+        calendarEventsPage.checkboxClickableAll.click();
+        Assert.assertTrue(calendarEventsPage.checkboxClickableAll.isSelected());
+
+        extentLogger.info("Verify that all calendar events were selected");
+        calendarEventsPage.verifyAllCheckbox();
+
+        extentLogger.pass("PASS: TestCase3-test5() Test");
+    }
 //
 //    @Test
 //    public void test6(){
