5d12371ab8c4f2256ec0eb38b4458e732dec03f9
davutbilgic <db.davutbilgic@gmail.com>
2020-05-27 21:51:18 +0200

    J class

---
+++ src/test/java/com/cybertek/tests/day19_ddf/DDFWithDataProvider.java
@@ -0,0 +1,27 @@
+package com.cybertek.tests.day19_ddf;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class DDFWithDataProvider {
+
+    @DataProvider
+    public Object [][] testData(){
+        String [][] data = {
+                {"Person of Interest","10"},
+                {"Kiling Eve","8"},
+                {"GoT","9"},
+                {"Breaking Bad","10"},
+                {"Dark","10"},
+                {"Dexter","9"},
+                {"Friends","10"}
+        };
+        return data;
+    }
+
+    @Test(dataProvider = "testData")
+    public void test1(String tvShow, String rating){
+
+        System.out.println("Tv show: " + tvShow + " has rating " + rating);
+    }
+}
